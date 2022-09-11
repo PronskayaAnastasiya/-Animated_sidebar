d40bce27b7db851360436ecac384dab860a26f4a
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-09-12 02:10:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(1);
+window.onload = function () {
+    let icon = document.querySelector('.sidebar__icon');
+    let main = document.querySelector('.main');
+    let main__content = document.querySelector('.main__content')
+
+    icon.onclick = function () {
+        this.classList.toggle('change__icon');
+        main.classList.toggle('main__bg')
+        main__content.classList.toggle('main__content__bg')
+    }
+    console.log(box, icon);
+};
